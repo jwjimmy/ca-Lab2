d03d503f17053ab2bc41c10644496375cf0b0957
Ian Daniher <it.daniher@gmail.com>
2012-10-28 21:39:47 -0400

    added prototypes for alu functionality

--- alu.v
+++ alu.v
@@ -3,4 +3,19 @@ module alu (busA, busB, ctrl, out, zero, ovf, carry);
 	input [31:0] busA, busB;
 	input [3:0] ctrl;
 	output zero, ovf, carry;
+
+	reg out;
+
+	always @ (busA, busB) begin
+		case (ctrl)
+			3'b000: assign out = busA + busB;
+			3'b001: assign out = busA - busB;
+			3'b010: assign out = busA ^ busB;
+			3'b011: assign out = (busA < busB);
+			3'b100: assign out = busA*busB;
+			3'b101: assign out = busA << busB;
+			3'b110: assign out = busA >> busB;
+			3'b111: assign out = busA >>> busB;
+		endcase
+	end
 endmodule
