3a2ae88acea5cda444f3093abc998d53a06d91c7
Ian Daniher <it.daniher@gmail.com>
2012-11-04 17:48:29 -0500

    added delay, s/spaces/tabs/

--- adder.v
+++ adder.v
@@ -1,25 +1,25 @@
 module Adder1Bit(
-    output reg out,
-    output reg cout,
-    input a,
-    input b
-    );
+	output reg out,
+	output reg cout,
+	input a,
+	input b
+	);
 
-    always @(a or b)
-    begin
-        {cout, out} = a + b;
-    end
+	always @(a or b)
+	begin
+		#10 {cout, out} = a + b;
+	end
 endmodule
 
 module Adder32Bit(
-    output reg[31:0] out,
-    output reg[31:0] cout,
-    input [31:0] a,
-    input [31:0] b
-    );
+	output reg[31:0] out,
+	output reg[31:0] cout,
+	input [31:0] a,
+	input [31:0] b
+	);
 
-    always @(a or b)
-    begin
-        {cout, out} = a + b;
-    end
+	always @(a or b)
+	begin
+		#320 {cout, out} = a + b;
+	end
 endmodule
