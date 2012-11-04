cf9604e6d815c44f357ca1039e7f99d6713e6d10
Ian Daniher <it.daniher@gmail.com>
2012-11-04 18:18:20 -0500

    s/spaces/tabs/

--- shiftTest.v
+++ shiftTest.v
@@ -1,20 +1,19 @@
 module test_shift;
-    reg [31:0] TinA, TinB;
-    wire [31:0] Tout;
+	reg [31:0] TinA, TinB;
+	wire [31:0] Tout;
 
-    initial
-    begin
-        $dumpfile("testsign.vcd");
-        $dumpvars(0,test_shift);
-        $monitor($time, , Tout, , TinA, ,TinB);
-        TinA = 32'b1;
+	initial
+	begin
+		$dumpfile("testsign.vcd");
+		$dumpvars(0,test_shift);
+		$monitor($time, , Tout, , TinA, ,TinB);
+		TinA = 32'b1;
 		TinB = 32'd2;
-        #100; 
+		#100; 
 		TinB = 32'd4;
 		#100;
 		TinB = 32'd8;
 		#100;
-    end
-
-    shift tester (Tout,TinA, TinB);
-endmodule    
+	end
+	shift tester (Tout,TinA, TinB);
+endmodule	
