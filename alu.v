beb7e49de20b5ac124dc7fdfc16f865bd619b30e
Ian Daniher <it.daniher@gmail.com>
2012-11-04 16:36:37 -0500

    added comments to clarify intended function of alu components

--- alu.v
+++ alu.v
@@ -8,14 +8,14 @@ module alu (busA, busB, ctrl, out, zero, ovf, carry);
 
 	always @ (busA, busB) begin
 		case (ctrl)
-			3'b000: #320 assign out = busA + busB;
-			3'b001: #340 assign out = busA - busB;
-			3'b010: #110 assign out = busA ^ busB;
-			3'b011: #120 assign out = (busA < busB);
-			3'b100: #10000 assign out = busA*busB;
-			3'b101: #150 assign out = busA << busB;
-			3'b110: #150 assign out = busA >> busB;
-			3'b111: #250 assign out = busA >>> busB;
+			3'b000: #320 assign out = busA + busB; // add
+			3'b001: #320 assign out = busA - busB; // subtract
+			3'b010: #320 assign out = busA ^ busB; // xor
+			3'b011: #640 assign out = (busA < busB); // set if less than
+			3'b100: #10000 assign out = busA*busB; // multiply
+			3'b101: #150 assign out = busA << busB; // leftshift 
+			3'b110: #150 assign out = busA >> busB; // rightshift
+			3'b111: #250 assign out = busA >>> busB; // arithmatic (sign extended) rightshift
 		endcase
 	end
 endmodule
