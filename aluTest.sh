7c66ba90b685c03b5fb90be08adc5731260219d2
JamesFW <james.wu@students.olin.edu>
2012-11-05 16:51:40 -0500

    Added aluTest verilog and sh files

--- aluTest.sh
+++ aluTest.sh
@@ -1,3 +1,4 @@
-iverilog -o aluTest.vvp alu.v aluTest.v
+iverilog -o aluTest.vvp alu.v aluTest.v adder.v sub.v slt.v shiftLeft.v
 vvp aluTest.vvp | tee data.txt
+gtkwave aluTest.vcd &
 rm aluTest.vvp 
--- aluTest.v
+++ aluTest.v
@@ -1,1 +1,19 @@
+module aluTest;
+	reg [31:0] TinA, TinB;
+	wire [31:0] Tout;
 
+	initial
+	begin
+		$dumpfile("aluTest.vcd");
+		$dumpvars(0,aluTest);
+		$monitor($time, , Tout, , TinA, ,TinB);
+		TinA = 32'b1;
+		TinB = 32'd2;
+		#500; 
+		TinB = 32'd4;
+		#500;
+		TinB = 32'd8;
+		#500;
+	end
+	alu aluTester (Tout,TinA, TinB);
+endmodule	
