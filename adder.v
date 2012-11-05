43097fd4fdae5ff85d9cb80a2ff2f8c52a47eabe
JamesFW <jamesfw@jw-Latitude-E6420.(none)>
2012-11-04 21:12:24 -0500

    redoing adder in structural verilog. it's 3 bits right now

--- adder.v
+++ adder.v
@@ -1,17 +1,34 @@
 module Adder1Bit(
-	output reg out,
-	output reg cout,
+	output out,
+	output cout,
 	input a,
-	input b
+	input b,
+    input cin
 	);
+    wire xorab, andcinx, andab;
 
-	always @(a or b)
-	begin
-		#10 {cout, out} = a + b;
-	end
+	//always @(a or b)
+	//begin
+        xor one(xorab,a,b); //2
+        xor two(out,xorab,cin); //2
+        and three(andcinx,xorab,cin); //2
+		and four(andab,a,b); //2
+		or five(cout,andcinx,andab); //2
+	//end
 endmodule
 
-module Adder32Bit(
+module Adder3Bit(
+	output [2:0] out,
+	output [2:0] cout,
+	input [2:0] a,
+	input [2:0] b
+	);
+    Adder1Bit bit0(out[0],cout[0],a[0],b[0],1'b0);
+    Adder1Bit bit1(out[1],cout[1],a[1],b[1],cout[0]);
+    Adder1Bit bit2(out[2],cout[2],a[2],b[2],cout[1]);
+endmodule
+
+module Adder32BitB(
 	output reg[31:0] out,
 	output reg[31:0] cout,
 	input [31:0] a,
--- adderTest.v
+++ adderTest.v
@@ -1,16 +1,21 @@
 module test_adder;
 	reg[31:0] Ta, Tb;
 	wire[31:0] Tout,Tcout;
+	reg[2:0] Taone, Tbone;
+	wire[2:0] Toutone, Tcoutone;
 	initial
 	begin
-		$monitor($time, , Tout, , Tcout, , Ta , ,Tb);
+		$monitor($time, , Toutone, , Tcoutone, , Taone , ,Tbone);
 
 		Ta = 32'b1000;
 		Tb = 32'b1001;
+		Taone = 3'b011;
+		Tbone = 3'b010;
 		#1000;
+		Tbone = 3'b001;
 		Ta = 32'b1011;
 		#1000;
 	end
-
-	Adder32Bit tester (Tout, Tcout, Ta,Tb);
+	Adder3Bit tester1bit (Toutone, Tcoutone, Taone, Tbone);
+	Adder32BitB tester (Tout, Tcout, Ta,Tb);
 endmodule 
