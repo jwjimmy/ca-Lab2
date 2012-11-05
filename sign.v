99f7f2f16068b022176b17c170a9ae8003f3d1c1
Ian Daniher <it.daniher@gmail.com>
2012-11-05 15:28:06 -0500

    added delay for 32 parallel NOT gates to sign.v

--- sign.v
+++ sign.v
@@ -2,7 +2,8 @@ module Sign32Bit(out,in);
     output [31:0] out;
     input [31:0] in;
     wire S32Bcout;
-	wire [31:0] temp = ~in; // thirty-two one-bit-wide 'not' gates in parallel - delay of ten
+	wire [31:0] temp;
+	assign #10 temp = ~in; // thirty-two one-bit-wide 'not' gates in parallel - delay of ten
 	Adder32Bit S32BAdder (out, S32Bcout, temp, 32'b1);
 endmodule
 
