07018c78728d67a215cc8ea515624d6160443483
JamesFW <james.wu@students.olin.edu>
2012-11-05 16:56:24 -0500

    Fixed the aluTest to work with the convention of alu.v

--- aluTest.v
+++ aluTest.v
@@ -1,6 +1,8 @@
 module aluTest;
 	reg [31:0] TinA, TinB;
 	wire [31:0] Tout;
+    reg [4:0] control;
+    wire zero, ovf, carry;
 
 	initial
 	begin
@@ -9,11 +11,12 @@ module aluTest;
 		$monitor($time, , Tout, , TinA, ,TinB);
 		TinA = 32'b1;
 		TinB = 32'd2;
+        control = 5'd2;
 		#500; 
 		TinB = 32'd4;
 		#500;
 		TinB = 32'd8;
 		#500;
 	end
-	alu aluTester (Tout,TinA, TinB);
+	alu aluTester (TinA, TinB, control, Tout, zero, ovf, carry);
 endmodule	
