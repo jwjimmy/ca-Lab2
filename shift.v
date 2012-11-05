63b06ead27595340e62eb87c757a11936f4eb40a
Ian Daniher <it.daniher@gmail.com>
2012-11-04 20:31:09 -0500

    rewrite shift.v in structural

--- shift.v
+++ shift.v
@@ -4,18 +4,21 @@ This module is constructed out of a single 32b wide shift register with a counte
 While slower than a mux/mux shifter, this is considerably smaller, and much easier to implement.
 */
 
+
+
 module shift(
-	output reg[31:0] out,
+	output reg [31:0] out,
 	input [31:0] inA, inB
 	);
 
-	reg[31:0] temp; // create 32b var 'temp'
+	reg [31:0] level0, level1, level2, level3, level4; // create 32b vars
 
-	always @ (inB, inA) begin // when inB or inA are changed
-		temp = inA; // temp gets inA
-		repeat( inB ) begin // for each in count(inB)
-			#10 temp = temp << 1; // shift the 32b number "temp" by one using a 32b wide shift register
-		end
-		out = temp; // out gets the value of temp
-	end 
+	always @ (inA, inB) begin
+		level0 = (inB&5'b00001) ? (inA << 1) : inA;
+		level1 = (inB&5'b00010) ? (level0 << 2) : level0;
+		level2 = (inB&5'b00100) ? (level1 << 4) : level1;
+		level3 = (inB&5'b01000) ? (level2 << 8) : level2;
+		level4 = (inB&5'b10000) ? (level3 << 16) : level3;
+		out = level4;
+	end
 endmodule
